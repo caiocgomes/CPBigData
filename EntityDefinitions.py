047af479ea74be5f6fa0bf7add22d2a68d45bc0b
Rafael S. Calsaverini <rafael.calsaverini@gmail.com>
2013-01-29 13:47:16 -0200

    adding another scheme

---
+++ EntityDefinitions.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from collections import namedtuple
+from UserGeneration import sexSampler, educationSampler, incomeSampler, ageGroupSampler
+
+class Product(namedtuple('Product', "prodid name category price") ):
+    _lastid = 0
+
+    def __new__(cls, name, category, price):
+        cls._lastid += 1
+        return super(Product, cls).__new__(cls, cls._lastid, name, category, price)
+
+class User(namedtuple('User', "userid sex education incomeGroup ageGroup")):
+    _lastid = 0
+
+    def __new__(cls):
+        cls._lastid += 1
+        sex = sexSampler()
+        ageGroup = ageGroupSampler()
+        education = educationSampler(ageGroup)
+        incomeGroup = incomeSampler(ageGroup, education)
+        return super(User, cls).__new__(cls, cls._lastid, sex, education, incomeGroup, ageGroup)
---
+++ UserGeneration.py
@@ -0,0 +1,28 @@
+from Util import categoricalSampler
+
+sexDistribution = {'M': 1, 'F': 1}
+ageGroupDistribution = {0: 1, 1: 3, 2: 2, 3: 1}
+
+def educationDistribution(ageGroup):
+    if ageGroup == 0:
+        return {0: 3, 1: 1}
+    elif ageGroup == 1:
+        return {0: 1, 1: 3}
+    elif ageGroup == 2:
+        return {0: 1, 1: 5, 2: 1}
+    elif ageGroup == 3:
+        return {0: 1, 1: 7, 2: 3}
+    elif ageGroup == 4:
+        return {0: 1, 1: 11, 2: 5}
+
+def incomeDistribution(age, edu):
+    return {0: 1, 1: 1 + 5 * edu * age, 2: 0.1 + edu * edu * age/2.0}
+
+sexSampler = categoricalSampler(sexDistribution)
+ageGroupSampler = categoricalSampler(ageGroupDistribution)
+
+def educationSampler(age):
+    return categoricalSampler(educationDistribution(age))()
+
+def incomeSampler(age, edu):
+    return categoricalSampler(incomeDistribution(age, edu))()
